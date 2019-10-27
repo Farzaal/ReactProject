04386f03621c9ac336bd995e9f5d0c57304446ee
Farzal <farzalkhan786@gmail.com>
2019-10-27 21:31:04 +0500

    Files removed

--- app/utils/injectReducer.js
+++ app/utils/injectReducer.js
@@ -1,32 +1,8 @@
 import React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
-import { ReactReduxContext } from 'react-redux';
 
-import getInjectors from './reducerInjectors';
-
-/**
- * Dynamically injects a reducer
- *
- * @param {string} key A key of the reducer
- * @param {function} reducer A reducer that will be injected
- *
- */
 export default ({ key, reducer }) => WrappedComponent => {
   class ReducerInjector extends React.Component {
-    static WrappedComponent = WrappedComponent;
-
-    static contextType = ReactReduxContext;
-
-    static displayName = `withReducer(${WrappedComponent.displayName ||
-      WrappedComponent.name ||
-      'Component'})`;
-
-    constructor(props, context) {
-      super(props, context);
-
-      getInjectors(context.store).injectReducer(key, reducer);
-    }
-
     render() {
       return <WrappedComponent {...this.props} />;
     }
@@ -35,11 +11,6 @@ export default ({ key, reducer }) => WrappedComponent => {
   return hoistNonReactStatics(ReducerInjector, WrappedComponent);
 };
 
-const useInjectReducer = ({ key, reducer }) => {
-  const context = React.useContext(ReactReduxContext);
-  React.useEffect(() => {
-    getInjectors(context.store).injectReducer(key, reducer);
-  }, []);
-};
+const useInjectReducer = {};
 
 export { useInjectReducer };
